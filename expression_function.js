5559601b95c2e346db119967e9651fc2e1cea58b
LMario98 <lo-coco-mario-@hotmail.it>
2025-06-04 12:36:57 +0200

    add current age calculatin function

--- expression_function.js
+++ expression_function.js
@@ -1,6 +1,6 @@
-// This function calculates the current age based on the year of birth..
+const yearsOfBorn = 1998;
+
 const currentAge = function (yearsOfBorn) {
   return  new Date().getFullYear() - yearsOfBorn;
 };
 
-  console.log(currentAge(1998));
