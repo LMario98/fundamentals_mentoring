2b20fd26e41f521aea5712588e6c624371a1ef16
LMario98 <lo-coco-mario-@hotmail.it>
2025-06-16 01:37:25 +0200

    add type validation for YearsOfBorn parameter in currentAge function

--- expression_function.js
+++ expression_function.js
@@ -1,8 +1,12 @@
 
 
 const currentAge = function (yearsOfBorn) {
+  if (typeof yearsOfBorn !== 'number') {
+    return "Only numbers are accepted in this function";
+  }
+   
   const todayYear = new Date().getFullYear();
   return  todayYear - yearsOfBorn;
 };
-
-currentAge(1998);
+console.log(currentAge("1998"));
+const foo = currentAge(1998);
