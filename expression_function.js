c31b25ee39a75633344b686d360d47e61c0718d7
LMario98 <lo-coco-mario-@hotmail.it>
2025-06-26 16:21:22 +0200

    refactor function and fix code

--- expression_function.js
+++ expression_function.js
@@ -4,7 +4,20 @@ const currentAge = function (yearsOfBorn) {
   if (typeof yearsOfBorn !== 'number') {
     return "Only numbers are accepted in this function";
   }
-   
+
   const todayYear = new Date().getFullYear();
-  return  todayYear - yearsOfBorn;
+  return todayYear - yearsOfBorn;
+};
+
+
+function currentAge1(yearsOfBorn1) {
+  if (typeof yearsOfBorn1 !== 'number') {
+    return '”Thanks for using my application in 2025!!, however can’t give you the age as you need to add a valid number”'
+
+  }
+
+  return 'Thanks for using my application in 2025!! your age today is 31'
+  
+  const todayYear = new Date().getFullYear();
+  return todayYear - yearsOfBorn1;
 };
