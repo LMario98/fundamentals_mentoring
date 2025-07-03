02ded91872a24bcb7fe26799d4a8d74692ef090d
LMario98 <lo-coco-mario-@hotmail.it>
2025-07-03 13:16:32 +0200

    refactor calculateCurrentAge function

--- expression_function.js
+++ expression_function.js
@@ -1,25 +1,10 @@
 
-
-const currentAge = function (yearsOfBorn) {
-  if (typeof yearsOfBorn !== 'number') {
-    return "Only numbers are accepted in this function";
-  }
-
-  const todayYear = new Date().getFullYear();
-  return todayYear - yearsOfBorn;
-};
-
-
-function currentAge1(yearsOfBorn1) {
-  if (typeof yearsOfBorn1 !== 'number') {
+function calculateCurrentAgeFromBirthYear(birthYear) {
+  if (typeof birthYear !== 'number') {
     return '”Thanks for using my application in 2025!!, however can’t give you the age as you need to add a valid number”'
 
   }
-  const calculateAge = new Date().getFullYear() - yearsOfBorn1;
+  const calculateAge = new Date().getFullYear() - birthYear;
   const getFullYear = new Date().getFullYear();
   return `Thanks for using my application in ${getFullYear} !! your age today is ${calculateAge}`;
-  
-  const todayYear = new Date().getFullYear();
-  return todayYear - yearsOfBorn1;
 };
-
