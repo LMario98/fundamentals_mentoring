885927913f6a15a2a75415b38b6fa8bf17331675
LMario98 <lo-coco-mario-@hotmail.it>
2025-07-18 11:53:07 +0200

    refactor calculateCurrentAgeFromBirthYear function to prompt in input

--- expression_function.js
+++ expression_function.js
@@ -1,14 +1,12 @@
+const startButton = document.getElementById('startButton');
+startButton.addEventListener("click", calculateCurrentAgeFromBirthYear);
 
 function calculateCurrentAgeFromBirthYear(birthYear) {
-  if (typeof birthYear !== 'number') {
-    return '”Thanks for using my application in 2025!!, however can’t give you the age as you need to add a valid number”'
-
-  }
-  const calculateAge = new Date().getFullYear() - birthYear;
-  const getFullYear = new Date().getFullYear();
-  return `Thanks for using my application in ${getFullYear} !! your age today is ${calculateAge}`;
+ const birthYear1 = prompt('insert a year')
+ return birthYear1
 };
 
+console.log(calculateCurrentAgeFromBirthYear());
 
 function getUserDetails(){
   return  currenAge = {
@@ -18,3 +16,4 @@ function getUserDetails(){
   }
 
 };
+
