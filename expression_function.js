1ba26ab3f5d48ec52405c0b53967de680b11e8f5
LMario98 <lo-coco-mario-@hotmail.it>
2025-08-03 11:49:51 +0200

    find_the_bug

--- expression_function.js
+++ expression_function.js
@@ -4,12 +4,16 @@ startButton.addEventListener("click", getUserDetails);
 
 function calculateCurrentAgeFromBirthYear() {
    const getBirthYear = prompt('insert a year')
-  if (typeof getBirthYear !== 'number') {
-     const calculateAge = new Date().getFullYear() - getBirthYear;
-    return calculateAge
-}
-};
+   const birthYearNumber = Number(getBirthYear);
+ 
 
+  if(isNaN(birthYearNumber)){
+    alert('hai inserito un numero non valido')
+    return 0
+  }
+   const calculateAge = new Date().getFullYear() - birthYearNumber;
+   return calculateAge
+};
 
 function getUserName() {
 const getUserName = prompt('insert your name');
