2bf94562c0326ea9583d872be3ce4ab113d2299c
LMario98 <lo-coco-mario-@hotmail.it>
2026-02-02 12:31:36 +0100

    Add input validation for non-numeric values

--- expression_function.js
+++ expression_function.js
@@ -8,5 +8,8 @@ const prompt2 = prompt("inserisci un numero");
 
 const convertPrompt2 = Number(prompt2);
 
-documentV.textContent = convertPrompt1 + convertPrompt2
+if(Number.isNaN(convertPrompt1) === true || Number.isNaN(convertPrompt2) === true){
+    console.log("puoi inserire perfavore un numero ");
 
+};
+documentV.textContent = convertPrompt1 + convertPrompt2
